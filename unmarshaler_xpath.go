6fa33a2eecb230c48e15e1038f6a85515ff9c9db
Paul Jiang <paul527@outlook.com>
2019-07-18 16:37:34 +0800

    支持http_proxy环境变量

--- unmarshaler_xpath.go
+++ unmarshaler_xpath.go
@@ -7,6 +7,8 @@ import (
 	"gopkg.in/xmlpath.v2"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 )
 
@@ -16,6 +18,11 @@ func init() {
 	transport := &http.Transport{
 		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
 		TLSHandshakeTimeout: time.Minute * 10,
+	}
+	proxy := os.Getenv("http_proxy")
+	proxyUrl, err := url.Parse(proxy)
+	if err != nil {
+		transport.Proxy = http.ProxyURL(proxyUrl)
 	}
 	DefaultHttpClient = &http.Client{Transport: transport, Timeout: time.Minute * 10}
 }
