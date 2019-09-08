0cffbfdcec555adbf231ce493ae76ad8a0a96ca8
Paul Jiang <paul527@outlook.com>
2019-09-08 23:55:58 +0800

    http_request_reader校验返回状态码

--- request_reader/http_request_reader.go
+++ request_reader/http_request_reader.go
@@ -5,12 +5,14 @@ import (
 	"github.com/x-armory/go-exception"
 	"io"
 	"net/http"
+	"strings"
 )
 
 var DefaultHttpRequestReader = &HttpRequestReader{util.DefaultHttpClient}
 
 type HttpRequestReader struct {
-	Client *http.Client
+	Ignore404 bool
+	Client    *http.Client
 }
 
 func (r *HttpRequestReader) ReadRequest(req interface{}) io.Reader {
@@ -20,5 +22,15 @@ func (r *HttpRequestReader) ReadRequest(req interface{}) io.Reader {
 	}
 	response, e := r.Client.Do(request)
 	ex.AssertNoError(e, "http do request failed")
+	ex.Assert(response.StatusCode < 500, "server error")
+	if response.StatusCode == 404 {
+		if !r.Ignore404 {
+			panic("404")
+		} else {
+			return strings.NewReader("")
+		}
+	} else {
+		ex.Assert(response.StatusCode < 400, response.Status)
+	}
 	return response.Body
 }
