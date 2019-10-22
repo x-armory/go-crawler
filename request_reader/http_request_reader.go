0ab251af038161242b7f7c28f52f7668adb88a4d
Paul Jiang <paul527@outlook.com>
2019-10-22 17:19:29 +0800

    增加http_request_reader调试日志

--- request_reader/http_request_reader.go
+++ request_reader/http_request_reader.go
@@ -1,9 +1,12 @@
 package request_reader
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/x-armory/go-crawler/util"
 	"github.com/x-armory/go-exception"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -11,8 +14,10 @@ import (
 var DefaultHttpRequestReader = &HttpRequestReader{Ignore404: true, Client: util.DefaultHttpClient}
 
 type HttpRequestReader struct {
-	Ignore404 bool
-	Client    *http.Client
+	Ignore404   bool
+	Client      *http.Client
+	LogRequest  bool
+	LogResponse bool
 }
 
 func (r *HttpRequestReader) ReadRequest(req interface{}) io.Reader {
@@ -20,8 +25,27 @@ func (r *HttpRequestReader) ReadRequest(req interface{}) io.Reader {
 	if !ok {
 		ex.Wrap("not supported parameter type").Throw()
 	}
+	if r.LogRequest {
+		var bodyBytes []byte
+		headerBytes, _ := json.Marshal(request.Header)
+		body, e := request.GetBody()
+		if e != nil {
+			println(e.Error())
+		} else {
+			bodyBytes, e = ioutil.ReadAll(body)
+			if e != nil {
+				println(e.Error())
+			}
+		}
+		println("Method", request.Method)
+		println("Url", request.URL.String())
+		println("Header", string(headerBytes))
+		println("Body", string(bodyBytes))
+	}
 	response, e := r.Client.Do(request)
 	ex.AssertNoError(e, "http do request failed")
+	ex.Assert(response.Body != nil, "response body is nil")
+	defer response.Body.Close()
 	ex.Assert(response.StatusCode < 500, "server error")
 	if response.StatusCode == 404 {
 		if !r.Ignore404 {
@@ -32,5 +56,11 @@ func (r *HttpRequestReader) ReadRequest(req interface{}) io.Reader {
 	} else {
 		ex.Assert(response.StatusCode < 400, response.Status)
 	}
-	return response.Body
+	responseBytes, e := ioutil.ReadAll(response.Body)
+	ex.AssertNoError(e, "read response error")
+	if r.LogResponse {
+		println("Response: %s", string(responseBytes))
+	}
+
+	return bytes.NewReader(responseBytes)
 }
